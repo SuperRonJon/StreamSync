66e3fe1204d959ab0cc3fe94ca51d01fd3fc6b1c
Patrick Seute <patrick.seute@cox.net>
2023-04-02 22:12:53 -0400

    add syncing of timestamped urls

--- main.py
+++ main.py
@@ -4,17 +4,56 @@ from secret import client_id as id, oauth_token as oauth
 from isodate import parse_duration
 
 import datetime
+import re
 
 
 client = TwitchClient(id, oauth)
 
 
-# Returns the real-world time that the clip occured at
-# @param vod: vod json data as returned from TwitchClient
-# @param offset: the number of seconds into the vod that the clip occured at
-def get_time_from_offset(vod, offset):
-    vod_creation = parser.parse(vod['created_at'])
-    return vod_creation + datetime.timedelta(seconds=offset)
+# Returns the real-world time a moment in a video occurred at
+# @param video_id: id of the video to be searched
+# @param offset: the number of seconds into the video that the clip occured at
+def get_time_from_offset(video_id, offset):
+    vod = client.get_video(video_id)
+    if vod is not None:
+        vod_creation = parser.parse(vod['created_at'])
+        return vod_creation + datetime.timedelta(seconds=offset)
+    return None
+
+
+# Returns the real-world time that the clip occurred at, or None if clip is not found
+# @param clip_slug: twitch clip slug string
+def get_time_from_clip(clip_slug):
+    clip = client.get_clip(clip_slug)
+    if clip is not None and clip['video_id']:
+        offset = clip['vod_offset']
+        video_id = clip['video_id']
+        return get_time_from_offset(video_id, offset)
+    return None
+
+
+# Returns the real-world time that a timestamped vod url occurred at, or None if no timestamp is found
+# @param vod_url: twitch timestamped vod url string
+def get_time_from_url(vod_url):
+    t = re.search(r"\?t=(.*)|&t=(.*)", vod_url)
+    v = re.search(r"videos\/(\d+)", vod_url)
+    if t is not None and v is not None:
+        timestamp = t.group(1) if t.group(1) else t.group(2)
+        offset = parse_duration("PT" + timestamp.upper()).seconds
+        video_id = v.group(1)
+        return get_time_from_offset(video_id, offset)  
+    return None
+
+
+# Returns the real-world time of a vod or clip slug, or None if no proper clip or url is found
+# @param url: clip slug or twitch timestamped vod url string
+def get_time_from_input(url):
+    url_time = get_time_from_url(url)
+    if url_time is not None:
+        return url_time
+    clip_time = get_time_from_clip(url)
+    return clip_time
+
 
 
 # Returns the timestamped URL for specified streamer at the given time
@@ -49,29 +88,22 @@ def get_timestamp_for_streamer(streamer, clip_time):
         return ""
 
 
-clip_slug = input("Enter clip slug: ")
-clip = client.get_clip(clip_slug)
-if clip is not None and clip['video_id']:
-    offset = clip['vod_offset']
-    op_vod = client.get_video(clip['video_id'])
-    if op_vod is not None:
-        clip_time = get_time_from_offset(op_vod, offset)
-
-        new_users = input("Enter streamers: ")
-        user_list = new_users.split()
-        for user in user_list:
-            output = get_timestamp_for_streamer(user, clip_time)
-            if output is not None:
-                if output:
-                    print("{}: {}".format(user, output))
-                else:
-                    print("{}: No vod found".format(user))
+url = input("Enter clip slug or timestamped vod url: ")
+clip_time = get_time_from_input(url)
+if clip_time is not None:
+    new_users = input("Enter streamers: ")
+    user_list = new_users.split()
+    for user in user_list:
+        output = get_timestamp_for_streamer(user, clip_time)
+        if output is not None:
+            if output:
+                print("{}: {}".format(user, output))
             else:
-                print("{}: Unable to find channel".format(user))
-    else:
-        print("Error getting vod clip is from")
-    input("Press Enter to continue...")
+                print("{}: No vod found".format(user))
+        else:
+            print("{}: Unable to find channel".format(user))
 else:
-    print('Error reading clip. Try again in a bit. The vod may not be available')
-    input("Press Enter to continue...")
-    
+    print("Error getting original vod/clip")
+input("Press Enter to continue...")
+
+
